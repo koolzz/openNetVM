a14534585caf3df835afd06646b6297ff277b9a0
root <root@node2.onvm-mos.gwcloudlab-pg0.wisc.cloudlab.us>
2020-06-01 18:12:56 -0500

    weicuidi

--- scripts/environment_wei.sh
+++ scripts/environment_wei.sh
@@ -3,14 +3,19 @@ sudo apt-get install build-essential linux-headers-$(uname -r) git
 sudo apt-get install libnuma-dev -y
 apt-get update
 
-cd /root/openNetVM
+#Path which may be modified
+#onvm-mos/core/src/Makefile.in needs to modify "ONVMPATH="
+ONVMPATH=/users/weicuidi/openNetVM
+ONVMMOSPATH=/users/weicuidi/onvm-mos
+
+cd $ONVMPATH
 git checkout pub_sub_nf_tracker
 echo export ONVM_HOME=$(pwd) >> ~/.bashrc
 
 cd
 git clone git@github.com:chandaweia/onvm-mos.git
 git checkout onvm-mos-new
-cd /root/onvm-mos/drivers/dpdk-18.11
+cd $ONVMMOSPATH/drivers/dpdk-18.11
 echo export RTE_SDK=$(pwd) >> ~/.bashrc
 echo export RTE_TARGET=x86_64-native-linuxapp-gcc >> ~/.bashrc
 echo export ONVM_NUM_HUGEPAGES=1024 >> ~/.bashrc
@@ -22,13 +27,13 @@ ifconfig enp6s0f1 down
 
 
 #install openNetVM
-cd /root/openNetVM/scripts
+cd $ONVMPATH/scripts
 ./install.sh
 
 #compile onvm
-cd /root/openNetVM/onvm
+cd $ONVMPATH/onvm
 make
 
 #onvm-mos
-cd /root/onvm-mos
+cd $ONVMMOSPATH
 ./setup.sh --compile-dpdk
